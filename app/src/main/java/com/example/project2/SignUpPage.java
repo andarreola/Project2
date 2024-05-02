a997f6b3152c34d1692eb72eb816af9f001273ad
Andrew Arreola <andarreola@csumb.edu>
2024-05-02 15:08:07 -0700

    Added sign up page functionality.

--- app/src/main/java/com/example/project2/SignUpPage.java
+++ app/src/main/java/com/example/project2/SignUpPage.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 //import androidx.core.graphics.Insets;
 //import androidx.core.view.ViewCompat;
 //import androidx.core.view.WindowInsetsCompat;
@@ -37,10 +39,45 @@ public class SignUpPage extends AppCompatActivity {
             }
         });
 
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userSignUp();
+            }
+        });
+
     }
 
     static Intent signUpPageFactory(Context context){
         Intent intent = new Intent(context, SignUpPage.class);
         return intent;
     }
+    //Sign up code below: Created 2:40pm
+    private void userSignUp(){
+        String username = binding.signUpUsernameEditText.getText().toString();
+        String password = binding.signUpPasswordEditText.getText().toString();
+        if(username.isEmpty() || password.isEmpty()){
+            toastMaker("Fields can not be empty.");
+            return;
+        }
+        LiveData<User> userObserver = repository.getUserByUserName(username);
+        userObserver.observe(this, user -> {
+            if(user != null){
+                //this checks if the user already exists in the database. If they do, it will make a toast letting the user know that the user already exists.
+                toastMaker(username + " is already taken");
+            } else {
+                //if the user does not exists, a new user will be added to the database.
+                repository.insertUser(new User(username, password));
+                toastMaker(username);
+                toastMaker(password);
+            }
+        });
+
+    }
+
+    private void toastMaker(String message){
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }

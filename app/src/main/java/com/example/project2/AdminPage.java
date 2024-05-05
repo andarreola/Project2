0911586c6b92c6a4b9fee7c6ee8d386fa7b2a999
Andrew Arreola <andarreola@csumb.edu>
2024-05-05 09:35:26 -0700

    Implemented button to go into admin page and out of admin page.

--- app/src/main/java/com/example/project2/AdminPage.java
+++ app/src/main/java/com/example/project2/AdminPage.java
@@ -1,25 +1,48 @@
 package com.example.project2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+import com.example.project2.database.UserRepository;
+import com.example.project2.database.entities.User;
+import com.example.project2.databinding.ActivityAdminPageBinding;
 
 public class AdminPage extends AppCompatActivity {
+
+    private static final String USERNAME_FROM_LANDING_KEY = "LandingPage_Activity_Username";
+    private UserRepository repository;
+    private User user = null;
+    private ActivityAdminPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_admin_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityAdminPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = UserRepository.getRepository(getApplication());
+
+        String username = getIntent().getStringExtra(USERNAME_FROM_LANDING_KEY);
+
+        binding.adminPageBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = LandingPage.landingPageIntentFactory(getApplicationContext(), username);
+                startActivity(intent);
+            }
         });
+
+    }
+
+    static Intent adminPageIntentFactory(Context context, String username){
+        Intent intent = new Intent(context, AdminPage.class);
+        intent.putExtra(USERNAME_FROM_LANDING_KEY, username);
+        return intent;
     }
 
 
